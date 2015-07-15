8848abc38e3868ff2175fc6c1ea4d02f6e725ace
garettarrowood <garettarrowood@gmail.com>
2015-07-15 08:36:04 -0400

    oops

--- lib/binary_handshake.rb
+++ lib/binary_handshake.rb
@@ -6,4 +6,40 @@
 # 1000 = "jump"
 # 10000 = Reverse the order of the operations in the secret handshake.
 
-# Write your code here.
+require "pry"
+
+class SecretHandshake
+
+	def initialize(num)
+		@num = num
+	end
+
+	def commands
+
+		answer = []
+
+		if @num[-1] == "1"
+			answer<<"wink"
+		end
+
+		if @num.length >= 2 && @num[-2] == "1"
+			answer<<"double blink"
+		end
+		
+		if @num.length >= 3 && @num[-3] == "1"
+			answer<<"close your eyes"
+		end
+
+		if @num.length >= 4 && @num[-4] == "1"
+			answer<<"jump"
+		end
+
+		if @num.length == 5
+			answer.reverse!
+	  end
+
+	  answer
+
+	end
+
+end

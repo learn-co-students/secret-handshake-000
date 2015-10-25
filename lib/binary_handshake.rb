b880b700bb9bb6b24712ba92cf899fb8c04bcfb3
NoneOfMaster <rcraigt@gmail.com>
2015-10-24 17:39:25 -0700

    funny handshake pun

--- lib/binary_handshake.rb
+++ lib/binary_handshake.rb
@@ -1,9 +1,38 @@
-# Binary Handshake Rules
+require 'pry'
 
-# 1 = "wink"
-# 10 = "double blink"
-# 100 = "close your eyes"
-# 1000 = "jump"
-# 10000 = Reverse the order of the operations in the secret handshake.
+class SecretHandshake
 
-# Write your code here.
+  Binary_Handshake_Rules = ["wink", "double blink", "close your eyes", "jump", "reverse"]
+  #{
+  #  1 => "wink",
+  #  10 => "double blink",
+  #  100 => "close your eyes",
+  #  1000 => "jump",
+  #  10000 => "reverse"
+  #}
+
+
+
+  def initialize(binary_handshake)
+    @binary_handshake = binary_handshake
+  end
+
+  def commands
+    rules_index = 0
+    rules = []
+    @binary_handshake.split("").reverse.each{|bit|
+      if bit == "1"
+        #binding.pry
+        rules << Binary_Handshake_Rules[rules_index]
+      end
+      rules_index += 1
+    }
+    if rules.include?("reverse")
+      rules.delete("reverse")
+      rules.reverse
+    else
+      rules
+    end
+  end
+
+end

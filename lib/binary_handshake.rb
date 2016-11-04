d2065fd3a4548290525784b88dd28cda0111682e
Lucas Fields <lqf2b8@gmail.com>
2016-11-04 22:40:03 +0000

    done

--- lib/binary_handshake.rb
+++ lib/binary_handshake.rb
@@ -1,9 +1,34 @@
-# Binary Handshake Rules
+class SecretHandshake
+	def initialize(code)
+		@handshake = []
+		@code_in_array = code.split('')
+		if @code_in_array.size == 5
+			@code_in_array.shift
+			@reverse = true
+		else
+			@reverse = false
+		end
 
-# 1 = "wink"
-# 10 = "double blink"
-# 100 = "close your eyes"
-# 1000 = "jump"
-# 10000 = Reverse the order of the operations in the secret handshake.
+		case @code_in_array.size
+		when 1
+			@handshake << "wink"
+		when 2
+			@handshake << "wink" if @code_in_array[1] == "1"
+			@handshake << "double blink" if @code_in_array[0] == "1"
+		when 3
+			@handshake << "close your eyes" if @code_in_array[0] == "1"
+			@handshake << "double blink" if @code_in_array[1] == "1"
+			@handshake << "wink" if @code_in_array[2] == "1"
+		when 4
+			@handshake << "jump" if @code_in_array[0] == "1"
+			@handshake << "close your eyes" if @code_in_array[1] == "1"
+			@handshake << "double blink" if @code_in_array[2] == "1"
+			@handshake << "wink" if @code_in_array[3] == "1"
+		end
+	end
 
-# Write your code here.
+	def commands
+		@handshake
+	end
+
+end 
